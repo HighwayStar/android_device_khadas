cf34d0c6d76a61e360ba782cdd1cc04ece32be60
Vitaliy Tomin <highwaystar.ru@gmail.com>
2020-06-28 22:13:16 +0800

    t962x2_skt: Rebrand kvim3l -> t962x2_skt
    * device tree for t962x2 (tl1_t962x2_skt)

--- t962x2_skt/vendorsetup.sh
+++ t962x2_skt/vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo kvim3l-eng
-add_lunch_combo kvim3l-user
-add_lunch_combo kvim3l-userdebug
+add_lunch_combo t962x2_skt-eng
+add_lunch_combo t962x2_skt-user
+add_lunch_combo t962x2_skt-userdebug
